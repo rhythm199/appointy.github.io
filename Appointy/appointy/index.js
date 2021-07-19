431d68c20e38a1bc211c00488958ef7a9cd271e7
rhythm199 <46573738+rhythm199@users.noreply.github.com>
2021-07-19 18:09:54 +0530

    my webpage

---
+++ Appointy/appointy/index.js
@@ -0,0 +1,11 @@
+console.time('your code took')
+console.log('1234');
+console.log(2*3);
+console.log(true)
+console.log([1,2,3,4])
+console.log({harry:'this',marks:34})
+console.table({harry:'this',marks:34})
+console.warn("this is warning");
+console.error('this is an error');
+console.timeEnd('your code took');
+//  console.clear();
